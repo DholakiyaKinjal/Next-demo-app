97fb5bb1d9d7ab54c8d71be2a72f5d717604a3ca
DholakiyaKinjal <kinjal.blurbee@gmail.com>
2025-02-27 13:33:49 +0530

    added wealthportal app

---
+++ apps/wealthportal/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-amber-400" appName="wealthportal">
+        <p>Hii</p>
+      </Button>
+    </div>
+  );
+}
